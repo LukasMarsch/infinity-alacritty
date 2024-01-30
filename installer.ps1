005ec119ea8b7326c96a39114e5469ac050329c5
Lukas Marsch <marschlukas@gmail.com>
2024-01-30 16:02:29 +0100

    installer update idk ob er worken tut

--- installer.ps1
+++ installer.ps1
@@ -1,33 +1,25 @@
 $alacrittyConfig = "~\AppData\Roaming\alacritty"
-$alacrittyInstaller = "~\Downloads\Alacritty-v0.12.2-installer.msi"
 
-Write-Host "Installing latest Powershell version please acknowledge and don't ask questions"
+Write-Host "Installing latest Powershell"
 winget install Microsoft.Powershell
 
-Write-Host "Installing latest Pansies version please acknowledge and don't ask questions"
-Install-Module Pansies -AllowClobber
+Write-Host "Installing latest Alacritty"
+winget install Allacritty.Alacritty
 
-Write-Host "Downloading alacritty config"
-if (!Test-Path $alacrittyConfig) {
-    mkdir $alacrittyConfig
-}
-
-Write-Host "Downloading Alacritty Profile, this will backup your old config"
 if(Test-Path $alacrittyConfig\alacritty.yml) {
     mv "$alacrittyConfig\alacritty.yml" "$alacrittyConfig\alacritty.yml.old"
-iwr -useb "https://raw.githubusercontent.com/LukasMarsch/infinity-alacritty/main/alacritty.yml" -Confirm | ni "$alacrittyConfig\alacritty.yml"
+    cp ".\alacritty.toml" "$alacrittyConfig\alacritty.toml"
+}
 
 Write-Host "Downloading Powershell Profile, we will backup your old config if you have one"
 if(Test-Path $profile) {
     mv $profile "$profile.old"
 }
-iwr -useb "https://raw.githubusercontent.com/LukasMarsch/infity-alacritty/main/profile.ps1" -Confirm | ni $profile
 
-if(!Test-Path "C:\Program Files\Alacritty\Alacritty.exe") {
-    Write-Host "Installing Alacritty"
-    iwr -useb "https://github.com/alacritty/alacritty/releases/download/v0.12.2/Alacritty-v0.12.2-installer.msi" | ni $alacrittyInstaller
-    Write-Host "Please Install Alacritty and follow the instructions in the installer"
-    Start-Process $alacrittyInstaller
-}
-Write-Host "Done"
 Start-Process alacritty
+if($(ps alacritty) -gt 0)
+    Stop-Process alacritty
+    Write-Host "Please install a nerdfont of your choice or the recommended JetBrains Nerdfont from the selection"
+    sleep -Seconds 4
+    Start-Process microsoft-edge:"https://www.nerdfonts.com/font-downloads"
+}
